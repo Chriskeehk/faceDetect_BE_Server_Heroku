b54be0ff9a2f44ef633bc81ed4db291365539648
unknown <chris@hkmouse.com>
2020-11-07 03:37:53 +0800

    Good

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const Clarifai = require('clarifai');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: {
-    	process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+    connectionString: process.env.DATABASE_URL,
+	ssl: {
+	    rejectUnauthorized: false
 	}
   }
 });
