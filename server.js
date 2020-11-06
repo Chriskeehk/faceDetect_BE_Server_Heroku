3f2f386ce2de9ef9d999b06fd44d95f60e771f20
unknown <chris@hkmouse.com>
2020-11-07 03:08:05 +0800

    Finish

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const Clarifai = require('clarifai');
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssl: true,
     // port : '5433',
     // user : 'postgres',
